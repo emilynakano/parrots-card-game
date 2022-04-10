f7db84359228f270107fed780e73cfe38baf759c
emilynak <emilynak@gmail.com>
2022-04-10 15:41:26 -0300

    distribuicao de cartas

--- scripts.js
+++ scripts.js
@@ -1,7 +1,51 @@
+let gifs = [
+    "images/bobrossparrot.gif",
+    "images/bobrossparrot.gif",
+    "images/explodyparrot.gif",
+    "images/explodyparrot.gif",
+    "images/fiestaparrot.gif",
+    "images/fiestaparrot.gif",
+    "images/metalparrot.gif",
+    "images/metalparrot.gif",
+    "images/revertitparrot.gif",
+    "images/revertitparrot.gif",
+    "images/tripletsparrot.gif",
+    "images/tripletsparrot.gif",
+    "images/unicornparrot.gif",
+    "images/unicornparrot.gif"
+]
 let qtd = prompt("quantas cartas?");
+while(qtd < 4 || qtd > 14) {
+    qtd = prompt("quantas cartas?");
+}
+
+gifs.length = qtd;
+gifs.sort(comparador); 
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
 let cont = 0;
 let container = document.querySelector(".container");
+
 while(cont < qtd) {
-    container.innerHTML = container.innerHTML + `<div class="carta"> <img src="images/front.png" /> </div>`;
+    container.innerHTML = container.innerHTML + `<div class="oi" onclick="virar(this)">
+    <div class="gif">
+        <img src="${gifs[cont]}" class="back-face" />
+    </div>
+    <div class="carta">
+        <img src="images/front.png" class="front-face" />
+    </div>
+    
+</div>
+
+`;
     cont ++;
 }
+
+
+
+
+
+
+
