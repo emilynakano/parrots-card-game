38924b28a6682b8c6d3aabd03167008edf670f39
emilynak <emilynak@gmail.com>
2022-04-10 22:48:37 -0300

    click

--- scripts.js
+++ scripts.js
@@ -24,6 +24,47 @@ gifs.sort(comparador);
 function comparador() { 
 	return Math.random() - 0.5; 
 }
+let firstCard;
+let secondCard;
+let jogada = 0;
+
+function virar(elemento) {
+    if(!firstCard) {
+        firstCard = elemento;
+    } else { 
+        secondCard = elemento;
+    }
+
+    firstCard.querySelector(".carta").classList.add("cartaSelecionada");
+    firstCard.querySelector(".gif").classList.add("gifSelecionada");
+    secondCard.querySelector(".carta").classList.add("cartaSelecionada");
+    secondCard.querySelector(".gif").classList.add("gifSelecionada");
+    
+    
+    
+    
+    if(firstCard.innerHTML === secondCard.innerHTML) {
+
+        secondCard.querySelector(".carta").classList.add("acartaSelecionada");
+        secondCard.querySelector(".gif").classList.add("agifSelecionada");
+        firstCard.querySelector(".carta").classList.add("acartaSelecionada");
+        firstCard.querySelector(".gif").classList.add("agifSelecionada");
+        
+
+    }
+    setTimeout( function() {
+        secondCard.querySelector(".carta").classList.remove("cartaSelecionada");
+        secondCard.querySelector(".gif").classList.remove("gifSelecionada");
+        firstCard.querySelector(".carta").classList.remove("cartaSelecionada");
+        firstCard.querySelector(".gif").classList.remove("gifSelecionada");
+        [firstCard, secondCard] = [null, null];
+      }, 1000);
+
+    jogada ++;
+}
+
+console.log(jogada);
+
 
 let cont = 0;
 let container = document.querySelector(".container");
